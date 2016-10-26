a817956e8a232f3bc68cef2336df87a60d4c7adb
Erica Levinsky <elevinsky@gmail.com>
2016-10-25 20:32:06 -0500

    single entry

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,8 +1,17 @@
 class GuessesController < ApplicationController
   def index
+
+    @first_guess = params["first_number"]
+    @second_guess = params["second_number"]
+    @third_guess = params["third_number"]
+
+
     render("guesses/index.html.erb")
   end
   def answer
+
+    @user_answer = params["rule"]
+
     render("guesses/answer.html.erb")
   end
 end
