6c9c2aef053025baed2ec27169bd18550af4099d
Erica Levinsky <elevinsky@gmail.com>
2016-10-25 21:01:11 -0500

    create a database

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,6 +1,14 @@
 class GuessesController < ApplicationController
   def index
 
+    g = Guess.new
+    g.first_num = params["first_number"]
+    g.second_num = params["second_number"]
+    g.third_num = params["third_number"]
+    g.save
+
+    @list = Guess.all
+
     @first_guess = params["first_number"]
     @second_guess = params["second_number"]
     @third_guess = params["third_number"]
