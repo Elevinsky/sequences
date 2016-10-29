c25be612a4f666a5e6c9da4d6d74fc414fe5b47e
Erica Levinsky <elevinsky@gmail.com>
2016-10-29 18:35:59 -0500

    trying to sync to other state

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -5,14 +5,10 @@ class GuessesController < ApplicationController
     g.first_num = params["first_number"]
     g.second_num = params["second_number"]
     g.third_num = params["third_number"]
+    if g.first_num != nil && g.second_num != nil && g.third_num != nil
     g.save
-
+    end
     @list = Guess.all
-
-    @first_guess = params["first_number"]
-    @second_guess = params["second_number"]
-    @third_guess = params["third_number"]
-
 
     render("guesses/index.html.erb")
   end
@@ -22,4 +18,10 @@ class GuessesController < ApplicationController
 
     render("guesses/answer.html.erb")
   end
+  def clear
+
+    Guess.destroy_all
+
+      render("guesses/index.html.erb")
+    end
 end
